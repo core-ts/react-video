f57c45d06d4f7ca6f54f7097660e0e3804d607f3
Duc Nguyen <minhduc140583@gmail.com>
2022-05-22 18:02:35 +0700

    Localize for filter

--- src/filter.ts
+++ src/filter.ts
@@ -25,40 +25,36 @@ export interface FilterBarProps {
 export const FilterBar = (props: FilterBarProps) => {
   return (React.createElement(React.Fragment, null,
     React.createElement('div', { className: 'filter-container ' + props.searchParams.get('filter') },
-        React.createElement('div', { className: 'filter-type' },
-            React.createElement('div', { className: 'filter-group-name' },
-                React.createElement('h4', null, props.resource ? props.resource.search_type : 'Search Type')),
-            // tslint:disable-next-line:only-arrow-functions
-            types.map(function (type) {
-                return React.createElement('div', { key: Object.keys(type)[0], className: props.filter.type === Object.keys(type)[0] ? 'selected' : '', onClick () { return props.handleFilterType(Object.keys(type)[0] === props.filter.type ? 'any' : Object.keys(type)[0] as any); } },
-                    (type as any)[Object.keys(type)[0]],
-                    ' ',
-                    props.filter.type === (Object.keys(type)[0]) && React.createElement(React.Fragment, null, '\u2716'));
-            })),
-        React.createElement('div', { className: 'filter-type' },
-            React.createElement('div', { className: 'filter-group-name' },
-                React.createElement('h4', null, props.resource ? props.resource.duration : 'Duration')),
-            // tslint:disable-next-line:only-arrow-functions
-            durations.map(function (type) {
-                return React.createElement('div', { key: Object.keys(type)[0], className: props.filter.duration === Object.keys(type)[0] ? 'selected' : '', onClick () { return props.handleFilterDuration(Object.keys(type)[0] === props.filter.duration ? 'any' : Object.keys(type)[0] as any); } },
-                    (type as any)[Object.keys(type)[0]],
-                    ' ',
-                    (type as any)[Object.keys(type)[0]],
-                    ' ',
-                    props.filter.duration === (Object.keys(type)[0]) && React.createElement(React.Fragment, null, '\u2716'));
-            })),
-        React.createElement('div', { className: 'filter-type' },
-            React.createElement('div', { className: 'filter-group-name' },
-                React.createElement('h4', null, props.resource ? props.resource.order : 'Order')),
-            // tslint:disable-next-line:only-arrow-functions
-            orders.map(function (type) {
-                return React.createElement('div', { key: Object.keys(type)[0], className: props.filter.order === Object.keys(type)[0] ? 'selected' : '', onClick () { return props.handleFilterOrder(Object.keys(type)[0] as any === props.filter.order ? 'relevance' : Object.keys(type)[0] as any); } },
-                    (type as any)[Object.keys(type)[0]],
-                    ' ',
-                    (type as any)[Object.keys(type)[0]],
-                    ' ',
-                    props.filter.order === (Object.keys(type)[0]) && React.createElement(React.Fragment, null, '\u2716'));
-            })))));
+      React.createElement('div', { className: 'filter-type' },
+        React.createElement('div', { className: 'filter-group-name' },
+          React.createElement('h4', null, props.resource ? props.resource.search_type : 'Search Type')),
+        // tslint:disable-next-line:only-arrow-functions
+        types.map(function (type) {
+          return React.createElement('div', { key: Object.keys(type)[0], className: props.filter.type === Object.keys(type)[0] ? 'selected' : '', onClick () { return props.handleFilterType(Object.keys(type)[0] === props.filter.type ? 'any' : Object.keys(type)[0] as any); } },
+            props.resource ? props.resource['type_' + Object.keys(type)[0]] : (type as any)[Object.keys(type)[0]],
+            ' ',
+            props.filter.type === (Object.keys(type)[0]) && React.createElement(React.Fragment, null, '\u2716'));
+        })),
+      React.createElement('div', { className: 'filter-type' },
+        React.createElement('div', { className: 'filter-group-name' },
+          React.createElement('h4', null, props.resource ? props.resource.duration : 'Duration')),
+        // tslint:disable-next-line:only-arrow-functions
+        durations.map(function (type) {
+          return React.createElement('div', { key: Object.keys(type)[0], className: props.filter.duration === Object.keys(type)[0] ? 'selected' : '', onClick () { return props.handleFilterDuration(Object.keys(type)[0] === props.filter.duration ? 'any' : Object.keys(type)[0] as any); } },
+            props.resource ? props.resource['video_' + Object.keys(type)[0]] : (type as any)[Object.keys(type)[0]],
+            ' ',
+            props.filter.duration === (Object.keys(type)[0]) && React.createElement(React.Fragment, null, '\u2716'));
+        })),
+      React.createElement('div', { className: 'filter-type' },
+        React.createElement('div', { className: 'filter-group-name' },
+          React.createElement('h4', null, props.resource ? props.resource.order : 'Order')),
+        // tslint:disable-next-line:only-arrow-functions
+        orders.map(function (type) {
+          return React.createElement('div', { key: Object.keys(type)[0], className: props.filter.order === Object.keys(type)[0] ? 'selected' : '', onClick () { return props.handleFilterOrder(Object.keys(type)[0] === props.filter.order ? 'relevance' : Object.keys(type)[0] as any); } },
+            props.resource ? props.resource['sort_' + Object.keys(type)[0]] : (type as any)[Object.keys(type)[0]],
+            ' ',
+            props.filter.order === (Object.keys(type)[0]) && React.createElement(React.Fragment, null, '\u2716'));
+        })))));
   /*
   return (
     <>
@@ -69,7 +65,7 @@ export const FilterBar = (props: FilterBarProps) => {
           </div>
           {types.map((type: any) =>
             <div key={Object.keys(type)[0]} className={props.filter.type === Object.keys(type)[0] ? 'selected' : ''} onClick={() => props.handleFilterType(Object.keys(type)[0] === props.filter.type ? 'any' : Object.keys(type)[0] as ItemType)}>
-              {type[Object.keys(type)[0]]} {props.filter.type === (Object.keys(type)[0]) && <>&#10006;</>}
+              {props.resource ? props.resource['type_' + Object.keys(type)[0]] : type[Object.keys(type)[0]]} {props.filter.type === (Object.keys(type)[0]) && <>&#10006;</>}
             </div>
           )}
         </div>
@@ -79,7 +75,7 @@ export const FilterBar = (props: FilterBarProps) => {
           </div>
           {durations.map((type: any) =>
             <div key={Object.keys(type)[0]} className={props.filter.duration === Object.keys(type)[0] ? 'selected' : ''} onClick={() => props.handleFilterDuration(Object.keys(type)[0] === props.filter.duration ? 'any' : Object.keys(type)[0] as Duration)}>
-              {type[Object.keys(type)[0]]} {type[Object.keys(type)[0]]} {props.filter.duration === (Object.keys(type)[0]) && <>&#10006;</>}
+              {props.resource ? props.resource['video_' + Object.keys(type)[0]] : type[Object.keys(type)[0]]} {props.filter.duration === (Object.keys(type)[0]) && <>&#10006;</>}
             </div>
           )}
         </div>
@@ -89,7 +85,7 @@ export const FilterBar = (props: FilterBarProps) => {
           </div>
           {orders.map((type: any) =>
             <div key={Object.keys(type)[0]} className={props.filter.order === Object.keys(type)[0] ? 'selected' : ''} onClick={() => props.handleFilterOrder(Object.keys(type)[0] === props.filter.order ? 'relevance' : Object.keys(type)[0] as SortType)}>
-              {type[Object.keys(type)[0]]} {type[Object.keys(type)[0]]} {props.filter.order === (Object.keys(type)[0]) && <>&#10006;</>}
+              {props.resource ? props.resource['sort_' + Object.keys(type)[0]] : type[Object.keys(type)[0]]} {props.filter.order === (Object.keys(type)[0]) && <>&#10006;</>}
             </div>
           )}
         </div>
